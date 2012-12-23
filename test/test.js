b3412affff8ceff9d6b530ffec871ecec9154eb4
Dustin Woodard <dustin.woodard@gmail.com>
2012-12-22 23:51:16 -0700

    more examples

--- test/test.js
+++ test/test.js
@@ -1,6 +1,20 @@
-module("main Tests");
+module("main Tests",{
+	setup: function  () {
+		equal( sayHello(), "hello", "one  assert before test");
+		this.prop = "foo";
+	},
+	teardown: function() {
+    equal( sayHello(), "hello", "and one extra assert after each test");
+  }
+});
+
 test( "a basic test example", function() {
-	expect( 1 );
-	equal( "hello", "hello", "We expect value to be hello" );
+	expect( 3 );
+	equal( sayHello(this.prop), "hello", "We expect value to be hello" );
+});
+
+test( "a basic test example", function() {
+	expect( 3 );
+	equal( sayHello(), "hello", "We expect value to be hello" );
 });
 
